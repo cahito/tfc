0f2ebc54cc35bfbca65af019f3e872f29b82cb61
cahito <cahito@gmail.com>
2022-08-17 20:19:52 -0300

    Feat - Requisito um cumprido. Criados o model e o migration do users.

---
+++ app/backend/src/database/models/user.ts
@@ -0,0 +1,49 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+  /**
+   * Helper method for defining associations.
+   * This method is not a part of Sequelize lifecycle.
+   * The `models/index` file will call this method automatically.
+   */
+  // static associate(models) {
+  // define association here
+  // }
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
