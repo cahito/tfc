859045bdb15954a6d45efb7da98c34e909c565f7
cahito <cahito@gmail.com>
2022-08-23 17:08:41 -0300

    Feat - Requisito dezoito cumprido. Models e migrations completas.

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,48 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+import IMatch from '../../interfaces/IMatch';
+
+class Match extends Model implements IMatch {
+  public id!: number;
+  public homeTeam: number;
+  public homeTeamGoals: number;
+  public awayTeam: number;
+  public awayTeamGoals: number;
+  public inProgress: number;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: INTEGER,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matchs',
+  timestamps: false,
+});
+
+export default Match;
