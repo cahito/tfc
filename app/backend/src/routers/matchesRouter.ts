95f02253a452ae765495fbf4f4b1556eec6da7ac
cahito <cahito@gmail.com>
2022-08-24 19:46:34 -0300

    Feat - Requisito vinte e cinco cumprido.

--- app/backend/src/routers/matchesRouter.ts
+++ app/backend/src/routers/matchesRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import validateToken from '../middlewares/validateToken';
 import MatchesController from '../controllers/MatchesController';
+import validateNotSameTeam from '../middlewares/validateNotSameTeam';
 
 const router = Router();
 const matchesController = new MatchesController();
 
 router.get('/', matchesController.list);
-router.post('/', validateToken, matchesController.create);
+router.post('/', validateToken, validateNotSameTeam, matchesController.create);
 router.patch('/:id/finish', matchesController.endGame);
 
 export default router;
